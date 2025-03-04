2cbf2de3f4b0ea7590859890108a5132340624a8
eduardo <eduardooost@gmail.com>
2025-03-04 09:04:43 -0400

    added global responsive tag

--- _/components/page_head/_.php
+++ _/components/page_head/_.php
@@ -40,6 +40,7 @@
             <!DOCTYPE html>
             <html lang="es">
             <head>
+               <meta name="viewport" content="width=device-width, initial-scale=1.0">
                <title>'.$this->page_title.'</title>
                <meta description="'.$page_description.'">
                <link rel="icon" type="image/x-icon" href="'.$this->root_folder.'_/media/favicon.ico">
